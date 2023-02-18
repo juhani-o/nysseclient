c2fed608207b01d94352f2c2bce0d3fbe6a4683b
Juhani Ovalo <juhani.ovalo@gmail.com>
2023-02-18 15:09:15 +0200

    Initial version and some initial work with header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <div className="flex flex-col items-center h-full justify-center min-h-screen py-2">
+    <Header />
+      <div className="w-6/12 rounded-lg bg-sky-300 min-h-full">
+        <p>moi</p>
+      </div>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
